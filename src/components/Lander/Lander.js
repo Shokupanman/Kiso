8712c7097712cd1d6bb2a44715901055b35051d9
Shokupanman <Harrisonkaidehancock@gmail.com>
2020-08-05 11:19:43 +0900

    basic build for lander

--- src/components/Lander/Lander.js
+++ src/components/Lander/Lander.js
@@ -3,6 +3,8 @@ import { MDBJumbotron, MDBCardTitle } from 'mdbreact'
 import './Lander.scss'
 import Logo from '../../Assets/Logo.png'
 import LogoAlt from '../../Assets/LogoAlt.png'
+import KisoW from '../../Assets/KisoW.png'
+import KisoR from '../../Assets/KisoR.png'
 import Jpn1 from '../../Assets/Jpn1.jpg'
 import Login from '../Auth/Login'
 
@@ -11,7 +13,8 @@ function Lander() {
     <div>
       <MDBJumbotron className="jumbo">
         <p className="jumboP">Learn like a Native</p>
-        <MDBCardTitle className="jumboT">Kiso</MDBCardTitle>
+        <img src={KisoR} alt="" className="logo" />
+        {/* <MDBCardTitle className="jumboT">Kiso</MDBCardTitle> */}
         <p className="jumboP">Speak like a Native</p>
       </MDBJumbotron>
       <section className="loginBit">
